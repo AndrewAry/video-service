e3cce3ddbc6b309e79f20e06c9ab4716088bd276
徐勇 <18361212610@163.com>
2018-07-21 22:53:34 +0800

    使用Swagger2

--- src/main/java/com/mwtutu/controller/UsersController.java
+++ src/main/java/com/mwtutu/controller/UsersController.java
@@ -3,6 +3,8 @@ package com.mwtutu.controller;
 
 import com.mwtutu.service.UsersService;
 import com.mwtutu.utils.ResStatus;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Api(value="用户相关业务的接口", tags= {"用户相关业务的controller"})
 public class UsersController {
 
     @Autowired
     private UsersService usersService;
 
+    @ApiOperation(value="获取用户列表", notes="获取用户列表接口")
     @GetMapping(value = "/user")
-    public ResStatus test(){
+    public ResStatus getUserList(){
         return ResStatus.ok(usersService.allUser());
     }
 }

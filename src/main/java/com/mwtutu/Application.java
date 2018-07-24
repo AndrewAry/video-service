6e949f23138b166e1c5133f319f8b52a6b467693
徐勇 <18361212610@163.com>
2018-07-24 16:46:42 +0800

    配置Druid连接池

--- src/main/java/com/mwtutu/Application.java
+++ src/main/java/com/mwtutu/Application.java
@@ -1,6 +1,8 @@
 package com.mwtutu;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -8,6 +10,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan(basePackages="com.mwtutu.mapper")
 @ComponentScan(basePackages= {"com.mwtutu"})
+@EnableAutoConfiguration(exclude={DruidDataSourceAutoConfigure.class})
 public class Application {
 
 	public static void main(String[] args) {

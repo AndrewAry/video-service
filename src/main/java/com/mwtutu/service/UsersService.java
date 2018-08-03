5e7c57f75445c5e2bf5f0d920916fcc30315553f
徐勇 <18361212610@163.com>
2018-08-03 14:44:14 +0800

    新增用户service层

--- src/main/java/com/mwtutu/service/UsersService.java
+++ src/main/java/com/mwtutu/service/UsersService.java
@@ -2,6 +2,7 @@ package com.mwtutu.service;
 
 import com.mwtutu.domain.UsersEvent;
 import com.mwtutu.mapper.UsersMapper;
+import com.mwtutu.utils.UUIDUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,22 @@ public class UsersService {
 
     public List<UsersEvent> allUser(){
         return usersMapper.selectAll();
+    }
+
+    public boolean queryUserNameIsExist(String username) {
+
+        UsersEvent usersEvent = new UsersEvent();
+        usersEvent.setUsername(username);
+
+        UsersEvent result = usersMapper.selectOne(usersEvent);
+
+        return result == null ? false : true;
+    }
+
+    public void  saveUser(UsersEvent usersEvent){
+        String userId = UUIDUtils.getUUID();
+        usersEvent.setId(userId);
+        usersMapper.insert(usersEvent);
     }
 
 }
